ac536d8a97e70fc49256a35e61806b3e533d220a
Matthew Evans <matt@matthewevansconsulting.com>
2019-04-27 11:37:34 -0500

    change http to https in StackOverflow API call

--- src/app/services/accomplishments.service.ts
+++ src/app/services/accomplishments.service.ts
@@ -12,7 +12,7 @@ export class AccomplishmentsService {
     constructor(private httpClient: HttpClient) { }
 
     public GetStackExchangeUser(name: string, site: string): Observable<StackExchange.User> {
-        const url = `http://api.stackexchange.com/2.2/users?order=desc&sort=reputation&inname=${name}&site=${site}`;
+        const url = `https://api.stackexchange.com/2.2/users?order=desc&sort=reputation&inname=${name}&site=${site}`;
         return this.httpClient.get<StackExchange.User>(url)
             .pipe();
     }
