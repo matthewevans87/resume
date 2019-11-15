73cf43d3c9979c4194dfb07b764afba1d93a9fc7
Matthew Evans <matthewevans87@gmail.com>
2019-11-15 15:46:39 -0600

    add az-900 cert; add isDisabled prop

--- src/app/models/resume.ts
+++ src/app/models/resume.ts
@@ -1,89 +1,90 @@
 export interface Term {
-    name?: string;
-    aliases?: string[];
-    url?: string;
+  name?: string;
+  aliases?: string[];
+  url?: string;
 }
 
-export interface Technology extends Term { }
+export interface Technology extends Term {}
 
 export interface NestedList {
-    description?: string;
-    children?: Accomplishment[];
+  description?: string;
+  children?: Accomplishment[];
 }
 
-export interface Accomplishment extends NestedList { }
+export interface Accomplishment extends NestedList {}
 
-export interface Responsibility extends NestedList { }
+export interface Responsibility extends NestedList {}
 
 export interface Project {
-    title?: string;
-    subTitle?: string;
-    description?: string;
-    distinguishedAttributes?: string[];
-    accomplishments?: Accomplishment[];
-    technologyNames?: string[];
+  title?: string;
+  subTitle?: string;
+  description?: string;
+  distinguishedAttributes?: string[];
+  accomplishments?: Accomplishment[];
+  technologyNames?: string[];
+  isDisabled: boolean;
 }
 
 export interface Testimonial {
-    body?: string;
-    author?: string;
-    authorUrl?: string;
-    position?: string;
-    team?: string;
-    company?: string;
+  body?: string;
+  author?: string;
+  authorUrl?: string;
+  position?: string;
+  team?: string;
+  company?: string;
 }
 
 export interface Position {
-    name?: string;
-    team?: string;
-    startYear?: number;
-    endYear?: number;
-    projects?: Project[];
-    responsibilities?: Responsibility[];
-    testimonials?: Testimonial[];
+  name?: string;
+  team?: string;
+  startYear?: number;
+  endYear?: number;
+  projects?: Project[];
+  responsibilities?: Responsibility[];
+  testimonials?: Testimonial[];
 }
 
 export interface Company {
-    name?: string;
-    city?: string;
-    state?: string;
-    url?: string;
-    positions?: Position[];
+  name?: string;
+  city?: string;
+  state?: string;
+  url?: string;
+  positions?: Position[];
 }
 
 export interface Institution {
-    name?: string;
-    city?: string;
-    state?: string;
-    url?: string;
-    certificates?: Certificate[];
+  name?: string;
+  city?: string;
+  state?: string;
+  url?: string;
+  certificates?: Certificate[];
 }
 
 export interface Certificate {
-    type?: string;
-    certName?: string;
-    certUrl?: string;
-    school?: string;
-    schoolUrl?: string;
-    date?: string;
+  type?: string;
+  certName?: string;
+  certUrl?: string;
+  school?: string;
+  schoolUrl?: string;
+  date?: string;
 }
 
 export interface AdditionalExperience {
-    personalProjects?: Project[];
+  personalProjects?: Project[];
 }
 
 export interface Candidate {
-    name: string;
-    location?: string;
-    locationUrl?: string;
-    phoneNumber?: string;
-    emailAddress?: string;
+  name: string;
+  location?: string;
+  locationUrl?: string;
+  phoneNumber?: string;
+  emailAddress?: string;
 }
 
 export interface Resume {
-    candidate: Candidate;
-    companies?: Company[];
-    leadStatements?: string[];
-    institutions?: Institution[];
-    additionalExperience?: AdditionalExperience;
+  candidate: Candidate;
+  companies?: Company[];
+  leadStatements?: string[];
+  institutions?: Institution[];
+  additionalExperience?: AdditionalExperience;
 }
